1e0ce74eb8f6d766ad6526be05696646c145fda9
vitor isaac sitton maia <vitor2001bidi@gmail.com>
2019-05-14 00:30:38 -0300

    agora vai3 sem bootstrap4

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('./dist/site-proj'));
 app.listen(process.env.PORT);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/site-proj/index.html'));
+    res.sendFile(path.join('./dist/site-proj/index.html'));
 });
 
 console.log('server is running');
