08861ff1d8ebbced6c345876be3495d267d8da06
Vitor Isaac Sitton Maia <vitor2001bidi@gmail.com>
2019-05-14 14:00:46 -0300

    agora vai 777

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('./dist/'));
 app.listen(process.env.PORT);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/site-proj/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/siteProj/index.html'));
 });
 
 console.log('server is running');
