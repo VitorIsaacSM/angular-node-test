623815cb0e7bc8708ea22df192c2912f7e1b5352
vitor isaac sitton maia <vitor2001bidi@gmail.com>
2019-05-13 23:43:11 -0300

    fifth  commit agora vai

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/site-proj'));
 
 app.listen(process.env.PORT);
 
 app.get('/*', (req, res => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/site-proj/index.html'));
 }));
 
 console.log('server is running')
